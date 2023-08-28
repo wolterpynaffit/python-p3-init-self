7dc898fed1eaf5a0331e9f4515037dece37ea678
Tiffany Pretlow <wolterpynaffit@yahoo.com>
2023-08-27 20:59:34 -0400

    finished

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed='Mutt'):
+        self.breed = breed
+        self.name = name
+
+    def bark(self):
+        print('Woof!')
+
+    def get_adopted(self, owner):
+        self.owner = owner
+        print(self.owner)
+
+
+fido = Dog('Snoopy')
+fido.get_adopted("Tim")
+fido.owner
